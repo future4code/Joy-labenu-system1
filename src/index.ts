7c07fb4e643426bc4c676b300971c18195fb4266
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2022-05-19 18:31:43 -0300

    adicionando a rota de criar turmas

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { app } from "./app";
 import { postEstudante } from "./endpoints/Estudante/postEstudante";
+import { postTurma } from "./endpoints/Turma/postTurma";
 
 app.post("/estudante", postEstudante)
+app.post("/turmas", postTurma)
